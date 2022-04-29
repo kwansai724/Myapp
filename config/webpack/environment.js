20594c4fadd5909661a6ded23a7925d6cd027d30
kwansai724 <kwansai.taka724@gmail.com>
2022-04-29 17:24:50 +0900

    first-commit

---
+++ config/webpack/environment.js
@@ -0,0 +1,29 @@
+const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+const { DefinePlugin } = require('webpack')
+
+environment.plugins.prepend(
+    'VueLoaderPlugin',
+    new VueLoaderPlugin()
+)
+
+environment.plugins.prepend(
+  'Define',
+  new DefinePlugin({
+    // __VUE_OPTIONS_API__: false,
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: false
+  })
+)
+
+environment.loaders.prepend('vue', {
+  test: /\.vue$/,
+  use: [{
+      loader: 'vue-loader'
+  }]
+})
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+module.exports = environment
